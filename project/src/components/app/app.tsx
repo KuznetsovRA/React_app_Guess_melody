84b1d599f83f8defe7b5c88199d5c52daca6503a
Kusnetsov Roman <KusnetsovRA@gmail.com>
2024-06-09 12:30:51 +0300

    Add mocks, state, GameScreen Delete service routes

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,27 +1,25 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { AppRoute, AuthorizationStatus } from '../../const';
 
-import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
 import AuthScreen from '../auth-screen/auth-screen';
 import GameOverScreen from '../game-over-screen/game-over-screen';
-import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
 import NotFoundScreen from '../not-found-screen/not-found-screen';
 import WelcomeScreen from '../welcome-screen/welcome-screen';
 import WinScreen from '../win-screen/win-screen';
 import PrivateRoute from '../private-route/private-route';
-
+import { Questions } from '../../types/question';
+import GameScreen from '../game-screen/game-screen';
 
 type AppScreenProps = {
   errorsCount: number;
+  questions: Questions;
 }
 
-function App({errorsCount}: AppScreenProps): JSX.Element {
+function App({errorsCount, questions}: AppScreenProps): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={AppRoute.Root} element={<WelcomeScreen errorsCount={errorsCount} />} />
-        <Route path={AppRoute.DevArtist} element={<ArtistQuestionScreen />} />
-        <Route path={AppRoute.DevGenre} element={<GenreQuestionScreen />} />
         <Route path={AppRoute.Login} element={<AuthScreen />} />
         <Route path={AppRoute.Result} element={
           <PrivateRoute
@@ -32,6 +30,12 @@ function App({errorsCount}: AppScreenProps): JSX.Element {
         }
         />
         <Route path={AppRoute.Lose} element={<GameOverScreen />} />
+        <Route path={AppRoute.Game} element={
+          <GameScreen
+            questions={questions}
+          />
+        }
+        />
         <Route path="*" element={<NotFoundScreen />} />
       </Routes>
     </BrowserRouter>
